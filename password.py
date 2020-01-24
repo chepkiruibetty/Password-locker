ab87bd8ebeae1de20e9c7fe9d2c38fa054ec1f98
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-01-24 12:35:06 +0300

    create two instance variables that take up the name and password

--- password.py
+++ password.py
@@ -2,5 +2,10 @@ class User:
     """
     Class that generates new instances of Users
     """
+def __init__(self,name,name_password):
 
-    pass
+    # docstring removed for simplicity
+
+        self.name = name
+        self.name_password =name_password
+       
