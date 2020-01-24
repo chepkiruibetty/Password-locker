c18ad2d3b2372d6599fe7b9f4bc83cd528f3897c
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-01-24 22:18:26 +0300

    solve errors using pycharm

--- password.py
+++ password.py
@@ -2,29 +2,41 @@ class User:
     """
     Class that generates new instances of Users
     """
-    user_list=[]
-    
+    user_list = []
+
     def __init__(self, name, user_password):
         self.name = name
         self.user_password = user_password
-    
+
     def save_user(self):
-        '''
+        """
         save_user method saves user objects into user_list
-        '''
+        """
         User.user_list.append(self)
-        
+
     def delete_user(self):
-        '''
+        """
         delete_user method deletes saved contact
-        '''
+        """
         User.user_list.remove(self)
-        
+
+
 class Credential:
-    '''
+    """
     Class that generates new instances of credentials
-    '''
-    def __init__(self,account,account_username,account_password):
-        self.account=account
-        self.account_username=account_username
-        self.account_password=account_password
+    """
+    credential_list = []
+
+    def __init__(self, account, account_username, account_password):
+        self.account = account
+        self.account_username = account_username
+        self.account_password = account_password
+
+    def save_credential(self):
+        """
+        save_credential method saves user objects into credential_list
+        """
+        Credential.credential_list.append(self)
+
+    # def test_save_credential(self):
+    #     pass
--- password_test.py
+++ password_test.py
@@ -1,69 +1,87 @@
 import unittest
-from password import User , Credential
+from password import User, Credential
+
 
 class TestUser(unittest.TestCase):
-    '''
+    """
     Test class that defines test cases for the user class behaviours.
 
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
-    '''
+    """
+
     def setUp(self):
-        self.new_user = User("Betty","12056")
-    
+        self.new_user = User("Betty", "12056")
+
     def tearDown(self):
         User.user_list = []
-    
+
     def test_init(self):
         self.assertEqual(self.new_user.name, "Betty")
         self.assertEqual(self.new_user.user_password, "12056")
-    
+
     def test_save_user(self):
-        '''
+        """
         test_save_user test case to test if the user object is saved into
         the user list
-        add 
-        '''
-        self.new_user.save_user() # saving the new users
+        add
+        """
+
+        self.new_user.save_user()  # saving the new users
         self.assertEqual(len(User.user_list), 1)
-    
+
     def test_save_multiple_user(self):
         self.new_user.save_user()
-        test_user = User("Gakii","12345")#new user
+        test_user = User("Gakii", "12345")  # new user
         test_user.save_user()
         self.assertEqual(len(User.user_list), 2)
-    
-def test_delete_user(self):
-    '''
-    test_delete_user to test if we can remove a contact from our users list
-    '''
-    self.new_user.save_user()
-    test_user = User("Gakii","12345") 
-    test_user.save_user()
 
-    self.new_user.delete_user()
-    self.assertEqual(len(User.user_list),1)
-    
+    def test_delete_user(self):
+        """
+        test_delete_user to test if we can remove a contact from our users list
+        """
+        self.new_user.save_user()
+        test_user = User("Gakii", "12345")
+        test_user.save_user()
+
+        self.new_user.delete_user()
+        self.assertEqual(len(User.user_list), 1)
+
+
 class TestCredential(unittest.TestCase):
-    '''
+    """
     Test class that defines test cases for the credential class behaviours.
 
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
-    '''
-    
+    """
+
     def setUp(self):
-        self.new_credential = Credential("facebook","bettyg","12345")
-        
+        self.new_credential = Credential("facebook", "bettyg", "12345")
+
+    def tearDown(self):
+        Credential.credential_list = []
+
     def test_init(self):
-        self.assertEqual(self.new_credential.account,"facebook")
-        self.assertEqual(self.new_credential.account_username,"bettyg")
-        self.assertEqual(self.new_credential.account_password,"12345")
-        
-        
-    
-    
-    
+        self.assertEqual(self.new_credential.account, "facebook")
+        self.assertEqual(self.new_credential.account_username, "bettyg")
+        self.assertEqual(self.new_credential.account_password, "12345")
+
+    def test_save_credential(self):
+        """
+        test_save_credential test case to test if the credential object is saved into
+        the credential list
+        add
+        """
+
+        self.new_credential.test_save_credential()  # saving the new credential
+        self.assertEqual(len(Credential.credential_list), 1)
+
+    def test_save_multiple_user(self):
+        self.new_credential.save_credential()
+        test_credential = Credential("Twitter","gakii", "123456")  # new credential
+        test_credential.save_credential()
+        self.assertEqual(len(Credential.credential_list), 2)
 
 
 if __name__ == '__main__':
