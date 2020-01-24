9f6b8e61c188a340521f73862c0161f3fafee4b9
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-01-24 12:57:52 +0300

    import unittest

--- password.py
+++ password.py
@@ -3,9 +3,6 @@ class User:
     Class that generates new instances of Users
     """
 def __init__(self,name,name_password):
-
-    # docstring removed for simplicity
-
-        self.name = name
-        self.name_password =name_password
-       
+    self.name = name
+    self.name_password =name_password
+    
