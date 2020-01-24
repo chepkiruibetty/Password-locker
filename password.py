c925abb9ce44e07b088bd7a0d97ebd588b719acd
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-01-24 14:17:18 +0300

    add setUp method to define instructions

--- password.py
+++ password.py
@@ -2,7 +2,7 @@ class User:
     """
     Class that generates new instances of Users
     """
-def __init__(self,name,name_password):
+def __init__(self,name,user_password):
     self.name = name
-    self.name_password =name_password
+    self.user_password = user_password
     
--- password_test.py
+++ password_test.py
@@ -1,1 +1,16 @@
 import unittest
+from password import User
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+def setUp(self):
+    self.new_user=User("Betty","12056")
+    
+def test_init(self):
+    self.assertEqual(self.new_user.user_name, "Betty")
+    self.assertEqual(self.new_user.user_password, "12056")
