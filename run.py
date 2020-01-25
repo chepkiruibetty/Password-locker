d85bd72885627a5dd515d34f50c1e13029eb0df4
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-01-25 13:08:03 +0300

    defined the functions that implement the behaviors in run.py

--- run.py
+++ run.py
@@ -1,2 +1,96 @@
 #!/usr/bin/env python3.6
-from user import User
+from password import User , Credential
+import random
+import string
+
+def create_user(name,user_password):
+    """
+    Parameters
+    ----------
+    name
+    user_password
+    Returns
+    -------
+    """
+    new_user = User(name, user_password)
+    return new_user
+
+def generate_password(user):
+    """
+    function to generate random password for user
+    ----------
+    user
+    Returns
+    -------
+    """
+    return user.generate_random_password()
+
+
+def save_user(user):
+    """
+    Function to save user
+    ----------
+    user
+    """
+    user.save_user()
+def delete_user(user):
+    """
+    Function to delete user
+    ----------
+    user
+    """
+    user.delete_user()
+def create_credential(account,account_username,account_password):
+    """
+    Parameters
+    ----------
+    name
+    credential_password
+    Returns
+    -------
+    """
+    new_credential = Credential(account,account_username,account_password)
+    return new_credential
+
+def save_credential(credential):
+    """
+    Function to save user
+    ----------
+    user
+    """
+    credential.save_credential() 
+    
+def delete_credential(credential):
+    """
+    Function to delete credential
+    ----------
+    credential
+    """
+    credential.delete_credential()  
+    
+def find_credential(account_username):
+    """
+    Function to find user
+    ----------
+    name
+    Returns
+    -------
+    """
+    return Credential.find_by_account_username(account_username)
+def check_existing_credentials(account_username):
+    """
+    Function to check existing credebtial
+    ----------
+    name
+    Returns
+    -------
+    user
+    """
+    return Credential.find_by_account_username(account_username)
+def display_credentials():
+    """
+    Function to display credential
+    Returns
+    -------
+    """
+    return Credential.display_all_credentials()
